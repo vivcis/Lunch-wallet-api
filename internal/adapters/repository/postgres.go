a27c3be9d110156b17d56f8efee232c052198f8f
Michael <michaelgbenle@gmail.com>
2022-07-08 14:35:10 +0100

    boiler

--- internal/adapters/repository/postgres.go
+++ internal/adapters/repository/postgres.go
@@ -1,1 +1,22 @@
 package repository
+
+import (
+	"fmt"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func ConnectDb(config *helpers.Config) (*gorm.DB, error) {
+	dsn := fmt.Sprintf("host=%v user=%v password=%v dbname=%v port=%v sslmode=%v TimeZone=%v", config.DBHost, config.DBUser, config.DBPass, config.DBName, config.DBPort, config.DBMode, config.DBTimeZone)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println("Established database connection")
+	return db, nil
+}
+
+func MigrateAll(db *gorm.DB) error {
+	return db.AutoMigrate(&models.FoodBeneficiary{}, &models.KitchenStaff{}, &models.Admin{})
+}
--- internal/adapters/repository/user.go
+++ internal/adapters/repository/user.go
@@ -1,1 +1,19 @@
 package repository
+
+import "gorm.io/gorm"
+
+type Postgres struct {
+	DB *gorm.DB
+}
+
+func NewUser(DB *gorm.DB) ports.UserRepository {
+	return &Postgres{DB}
+}
+
+func (p *Postgres) GetByID(id string) (*models.User, error) {
+	var user models.User
+	if err := p.DB.Where("id = ?", id).First(&user).Error; err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
