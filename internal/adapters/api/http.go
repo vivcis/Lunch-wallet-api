85fcc3128c068a83ff2651f024d899e57a196532
Michael <michaelgbenle@gmail.com>
2022-07-08 14:30:44 +0100

    boiler

--- internal/adapters/api/http.go
+++ internal/adapters/api/http.go
@@ -1,1 +1,11 @@
 package api
+
+type HTTPHandler struct {
+	UserService ports.UserService
+}
+
+func NewHTTPHandler(userService ports.UserService) *HTTPHandler {
+	return &HTTPHandler{
+		UserService: userService,
+	}
+}
--- internal/adapters/api/user.go
+++ internal/adapters/api/user.go
@@ -1,1 +1,28 @@
 package api
+
+import (
+	"errors"
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+	"net/http"
+)
+
+func (u HTTPHandler) GetByID(c *gin.Context) {
+	id := c.Param("id")
+	if err := c.ShouldBindUri(id); err != nil {
+		helpers.JSON(c, "", http.StatusBadRequest, nil, []string{err.Error()})
+		return
+	}
+
+	user, err := u.UserService.GetByID(id)
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			helpers.JSON(c, "User with the id does not exist", http.StatusNotFound, nil, []string{err.Error()})
+			return
+		}
+		helpers.JSON(c, "Error getting user", http.StatusInternalServerError, nil, []string{err.Error()})
+		return
+	}
+
+	helpers.JSON(c, "User found successfully", http.StatusOK, user, nil)
+}
