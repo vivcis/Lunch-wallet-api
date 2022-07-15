85ea98e94dbf710c661c671aae8c3966562aa48b
Kjasuquo <joseph.asuquo@decagon.dev>
2022-07-15 13:49:32 +0100

    troubleshooting in error file

--- internal/core/helpers/error.go
+++ internal/core/helpers/error.go
@@ -1,5 +1,7 @@
 package helpers
 
+import "fmt"
+
 type InActiveUserError struct {
 	err string
 }
@@ -8,5 +10,6 @@ func (i InActiveUserError) Error() string {
 	if i.err == "" {
 		i.err = "user is inactive"
 	}
+	fmt.Println("erroroooooo")
 	return i.err
 }
