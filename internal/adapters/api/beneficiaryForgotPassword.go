4fe62c38b65739f5f2251a60da5630503d06607c
Michael <michaelgbenle@gmail.com>
2022-07-15 16:04:28 +0100

    forgot password

--- internal/adapters/api/beneficiaryForgotPassword.go
+++ internal/adapters/api/beneficiaryForgotPassword.go
@@ -3,8 +3,16 @@ package api
 import (
 	"github.com/decadevs/lunch-api/internal/core/models"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func (u HTTPHandler) FoodBeneficiaryForgotPassword(c *gin.Context) {
 	var forgotPassword models.ResetPasswordRequest
+
+	err := c.ShouldBindJSON(&forgotPassword)
+	if err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "please fill all fields"})
+
+		return
+	}
 }
