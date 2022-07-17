d0de30dee9b4306ba7193d9ffe3c2374799f59f0
Michael <michaelgbenle@gmail.com>
2022-07-17 16:02:31 +0100

    debit refactored

--- internal/adapters/api/tests/forgotPassword_test.go
+++ internal/adapters/api/tests/forgotPassword_test.go
@@ -1,12 +1,17 @@
 package tests
 
 import (
+	"encoding/json"
 	"github.com/decadevs/lunch-api/cmd/server"
 	"github.com/decadevs/lunch-api/internal/adapters/api"
 	"github.com/decadevs/lunch-api/internal/adapters/repository/mocks"
 	"github.com/decadevs/lunch-api/internal/core/models"
 	"github.com/golang/mock/gomock"
+	"log"
+	"net/http"
+	"net/http/httptest"
 	"os"
+	"strings"
 	"testing"
 )
 
@@ -40,5 +45,13 @@ func TestBuyerSendForgotPasswordEMailHandler(t *testing.T) {
 	mockDb.EXPECT().FindFoodBenefactorByEmail(resetPassword.Email).Return(beneficiary, nil)
 	Link := "<strong>Here is your reset <a href='http://localhost:8080/api/v1/user/beneficiaryresetpassword/cad4fc7b-b819-4ec0-aff4-5cefefd7f8ee'>link</a></strong>"
 	mockMail.EXPECT().SendMail("forgot Password", Link, "test@gmail.com", privateAPIKey, yourDomain).Return(nil)
+	resetPasswordPayload, err := json.Marshal(resetPassword)
+	if err != nil {
+		log.Println(err)
+		t.Fail()
+	}
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("POST", "/api/v1/buyer/forgotpassword",
+		strings.NewReader(string(resetPasswordPayload)))
 
 }
