e1580d1b05a212cbcff64b91a08e5106c343ae2a
Michael <michaelgbenle@gmail.com>
2022-07-17 15:08:37 +0100

    forgot password

--- internal/adapters/api/tests/forgotPassword_test.go
+++ internal/adapters/api/tests/forgotPassword_test.go
@@ -1,27 +1,23 @@
 package tests
 
 import (
-	"github.com/decadevs/lunch-api/cmd/server"
-	"github.com/decadevs/lunch-api/internal/adapters/api"
-	"github.com/decadevs/lunch-api/internal/adapters/repository/mocks"
-	"github.com/golang/mock/gomock"
 	"testing"
 )
 
 func TestBuyerSendForgotPasswordEMailHandler(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	mockDb := mocks.NewMockUserRepository(ctrl)
+	//ctrl := gomock.NewController(t)
+	//mockDb := mocks.NewMockUserRepository(ctrl)
 
-	r := &api.HTTPHandler{
-		UserService: mockDb,
-	}
+	//r := &api.HTTPHandler{
+	//	UserService: mockDb,
+	//}
 
-	router := server.SetupRouter(r, mockDb)
-
-	resetPassword := struct {
-		Email string `json:"email"`
-	}{
-		Email: "test@testmail.com",
-	}
+	//router := server.SetupRouter(r, mockDb)
+	//
+	//resetPassword := struct {
+	//	Email string `json:"email"`
+	//}{
+	//	Email: "test@testmail.com",
+	//}
 
 }
