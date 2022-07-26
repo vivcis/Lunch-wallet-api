a7821ec20c00da3d2db9f59fcda04aa300cb3224
vivcis <ceciliaorji.co@gmail.com>
2022-07-26 13:03:26 +0100

    Modify getusers.go

--- internal/adapters/api/getusers.go
+++ internal/adapters/api/getusers.go
@@ -1,5 +1,14 @@
-package main
+package api
 
-func main() {
-	$END$
+import (
+	"github.com/decadevs/lunch-api/internal/core/helpers"
+	"github.com/gin-gonic/gin"
+)
+
+func (u *HTTPHandler) GetUser(c *gin.Context) {
+	_, err := u.GetBenefactorFromContext(c)
+	if err != nil {
+		helpers.JSON(c, "internal server error", 500, nil, []string{"internal server error"})
+		return
+	}
 }
