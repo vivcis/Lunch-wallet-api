c9aca08a068473a9eb1284ee47cd226361c4b5ca
vivcis <ceciliaorji.co@gmail.com>
2022-07-26 13:02:51 +0100

    refactor main.go

--- cmd/main.go
+++ cmd/main.go
@@ -2,18 +2,10 @@ package main
 
 import (
 	"github.com/decadevs/lunch-api/cmd/server"
-	"github.com/joho/godotenv"
 	"log"
-	"os"
 )
 
 func main() {
-	env := os.Getenv("GIN_MODE")
-	if env != "release" {
-		if err := godotenv.Load(); err != nil {
-			log.Fatalf("couldn't load env vars: %v", err)
-		}
-	}
 	db, err := server.Run()
 	if err != nil {
 		log.Fatal(err)
