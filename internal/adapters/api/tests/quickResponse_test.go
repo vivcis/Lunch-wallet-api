6e1be5397f98c9de4ee391c32938be5447c75221
Michael <michaelgbenle@gmail.com>
2022-07-29 20:40:08 +0100

    qr code working

--- internal/adapters/api/tests/quickResponse_test.go
+++ internal/adapters/api/tests/quickResponse_test.go
@@ -4,8 +4,11 @@ import (
 	"github.com/decadevs/lunch-api/cmd/server"
 	"github.com/decadevs/lunch-api/internal/adapters/api"
 	"github.com/decadevs/lunch-api/internal/adapters/repository/mocks"
+	"github.com/decadevs/lunch-api/internal/core/models"
 	"github.com/golang/mock/gomock"
+	"gorm.io/gorm"
 	"testing"
+	"time"
 )
 
 func TestBeneficiaryQRBrunch(t *testing.T) {
@@ -16,4 +19,19 @@ func TestBeneficiaryQRBrunch(t *testing.T) {
 		UserService: mockDb,
 	}
 	router := server.SetupRouter(r, mockDb)
+	model := models.Model{
+		ID:        "hello",
+		CreatedAt: time.Time{},
+		UpdatedAt: time.Time{},
+		DeletedAt: gorm.DeletedAt{},
+	}
+	date := time.Now().Format("2006-01-02")
+	mealRecord := &models.MealRecords{
+		Model:     model,
+		MealDate:  date,
+		UserID:    "",
+		UserEmail: "",
+		Brunch:    false,
+		Dinner:    false,
+	}
 }
