32ea9e2b472b023333065b7d4be08f68d6aea8aa
vivcis <ceciliaorji.co@gmail.com>
2022-07-31 14:02:09 +0100

    Refactor changestatus.go

--- internal/adapters/api/changestatus.go
+++ internal/adapters/api/changestatus.go
@@ -1,49 +1,20 @@
 package api
 
 import (
-	"fmt"
 	"github.com/decadevs/lunch-api/internal/core/helpers"
-	"github.com/decadevs/lunch-api/internal/core/models"
 	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
 	"time"
 )
 
-//func (u *HTTPHandler) ChangeFoodStatus(c *gin.Context) {
-//	user, err := u.GetFoodFromContext(c)
-//	if err != nil {
-//		helpers.JSON(c, "An internal server error", 500, nil, []string{"internal server error"})
-//		return
-//	}
-//	status := c.Param("status")
-//	Id := c.Param("id")
-//	foodStatus, err := helpers.CheckFoodStatus(status)
-//	if err != nil {
-//		helpers.JSON(c, "Bad request", 400, nil, []string{"bad request"})
-//		return
-//	}
-//	food, er := u.UserService.GetFoodByID(Id)
-//	if er != nil {
-//		helpers.JSON(c, "This is an internal server error", 500, nil, []string{"internal server error"})
-//		return
-//	}
-//	foodError := u.UserService.UpdateFoodStatusById(food.ID, foodStatus)
-//	if foodError != nil {
-//		helpers.JSON(c, "internal server error", 500, nil, []string{"internal server error"})
-//		return
-//	}
-//	helpers.JSON(c, "food status updated successfully", http.StatusOK, user, nil)
-//}
-
-func (u *HTTPHandler) UpdateFoodStatus(c *gin.Context) {
+func (u *HTTPHandler) UpdateBrunchFoodStatus(c *gin.Context) {
 	user, err := u.GetKitchenStaffFromContext(c)
 	log.Println("user in context", user)
 	if err != nil {
 		helpers.JSON(c, "An internal server error", 500, nil, []string{"internal server error"})
 		return
 	}
-
 	year, month, day := time.Now().Date()
 
 	food, err := u.UserService.FindBrunchByDate(year, month, day)
@@ -51,28 +22,60 @@ func (u *HTTPHandler) UpdateFoodStatus(c *gin.Context) {
 		helpers.JSON(c, "internal server error", 500, nil, []string{"internal server error"})
 		return
 	}
-	//status := c.Param("status")
-	//Id := c.Param("id")
-	//foodId, _ := strconv.Atoi(Id)
-	//foodID := uint(foodId)
 
-	var food = models.Food{}
-	if err := c.BindJSON(&food); err != nil {
+	type StatusUpdate struct {
+		Status string `json:"status"`
+	}
+
+	var status StatusUpdate
+
+	if err := c.BindJSON(&status); err != nil {
 		helpers.JSON(c, "An internal server error", 500, nil, []string{"internal server error"})
 		return
 	}
-	foodRecord, statusError := u.UserService.GetFoodByID(user.ID)
-	if statusError != nil {
-		helpers.JSON(c, "An internal server error", 500, nil, []string{"Food does not exist"})
-		return
-	}
 
-	fmt.Println(food.Name)
-
-	errS := u.UserService.UpdateFoodStatusById(foodRecord, status)
+	errS := u.UserService.UpdateStatus(food, status.Status)
 	if errS != nil {
 		helpers.JSON(c, "An internal server error", 500, nil, []string{"error updating food"})
 		return
 	}
-	helpers.JSON(c, "food status updated successfully", http.StatusOK, food, nil)
+
+	currentFood, _ := u.UserService.FindBrunchByDate(year, month, day)
+	helpers.JSON(c, "food status updated successfully", http.StatusOK, currentFood, nil)
+}
+
+func (u *HTTPHandler) UpdateDinnerFoodStatus(c *gin.Context) {
+	user, err := u.GetKitchenStaffFromContext(c)
+	log.Println("user in context", user)
+	if err != nil {
+		helpers.JSON(c, "An internal server error", 500, nil, []string{"internal server error"})
+		return
+	}
+	year, month, day := time.Now().Date()
+
+	food, err := u.UserService.FindDinnerByDate(year, month, day)
+	if err != nil {
+		helpers.JSON(c, "internal server error", 500, nil, []string{"internal server error"})
+		return
+	}
+
+	type StatusUpdate struct {
+		Status string `json:"status"`
+	}
+
+	var status StatusUpdate
+
+	if err := c.BindJSON(&status); err != nil {
+		helpers.JSON(c, "internal server error", 500, nil, []string{"internal server error"})
+		return
+	}
+
+	errS := u.UserService.UpdateStatus(food, status.Status)
+	if errS != nil {
+		helpers.JSON(c, "internal server error", 500, nil, []string{"error updating food"})
+		return
+	}
+
+	currentFood, _ := u.UserService.FindDinnerByDate(year, month, day)
+	helpers.JSON(c, "food status updated successfully", http.StatusOK, currentFood, nil)
 }
