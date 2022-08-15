bf9ddc4ead1cb7c546cbb93d248791260db6c607
majorchork <majorchork@gmail.com>
2022-08-15 01:53:20 +0100

    Feat: Admin Dashboard Handlers and Routes

--- cmd/server/route.go
+++ cmd/server/route.go
@@ -90,6 +90,11 @@ func SetupRouter(handler *api.HTTPHandler, userService ports.UserService) *gin.E
 		authorizeAdmin.DELETE("/removefoodbeneficiary/:id", handler.RemoveFoodBeneficiary)
 		authorizeAdmin.GET("/numberblocked", handler.GetNumberOfBlockedUsers)
 		authorizeAdmin.GET("/blockedusers", handler.GetBlockedUsers)
+		authorizeAdmin.GET("/getTimetable", handler.GetMealTimetableHandle)
+		authorizeAdmin.GET("/getAllBeneficiaries", handler.GetAllBeneficiaryHandle)
+		authorizeAdmin.GET("/searchBeneficiaries", handler.AdminSearchFoodBeneficiaries)
+		authorizeAdmin.GET("/getTotalNumberOfUsers", handler.AdminGetTotalNumberOfUsers)
+
 	}
 	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
