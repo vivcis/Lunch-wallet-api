08dc0fb1fd2e2574906d5cad92a8930342ba33cf
vivcis <ceciliaorji.co@gmail.com>
2022-08-17 00:49:20 +0100

    Add documentation to getusers.go

--- internal/adapters/api/getusers.go
+++ internal/adapters/api/getusers.go
@@ -12,6 +12,16 @@ func (u *HTTPHandler) PingHandler(c *gin.Context) {
 	helpers.JSON(c, "pong", 200, nil, nil)
 }
 
+// GetFoodBeneficiaries godoc
+// @Summary      GetsFood Beneficiaries
+// @Description  Kitchen staff gets list of food beneficiaries. It is an authorized route to only KITCHEN STAFF
+// @Tags         Users
+// @Accept       json
+// @Produce      json
+// @Success      200  {number} string "food beneficiaries found successfully"
+// @Failure      500  {string}  string "internal server error"
+// @Failure      400  {string}  string "bad request"
+// @Router       /staff/getusers [get]
 func (u *HTTPHandler) GetFoodBeneficiaries(c *gin.Context) {
 	_, err := u.GetKitchenStaffFromContext(c)
 	if err != nil {
