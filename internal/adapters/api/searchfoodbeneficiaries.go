bbcec5ae85231a41ed25af6bdc2a347e382ce6ed
vivcis <ceciliaorji.co@gmail.com>
2022-08-17 10:47:30 +0100

    Refactor searchfoodbeneficiaries.go doc

--- internal/adapters/api/searchfoodbeneficiaries.go
+++ internal/adapters/api/searchfoodbeneficiaries.go
@@ -16,7 +16,7 @@ import (
 // @Success      200  {number} string "information gotten"
 // @Failure      500  {string}  string "internal server error"
 // @Failure      400  {string}  string "bad request"
-// @Router       /staff/searchbeneficiary/:text [get]
+// @Router       /staff/searchbeneficiary/{text}[get]
 func (u *HTTPHandler) SearchFoodBeneficiaries(c *gin.Context) {
 	_, err := u.GetKitchenStaffFromContext(c)
 	if err != nil {
